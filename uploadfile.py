4724f5608d7a3a5fd9915d6311179643c6eb537c
Kastro .R <kastrouchiha@gmail.com>
2025-01-10 08:27:09 -0800

    Add files via upload
    "Visionary Lens: AI-Powered Image Content Detection"
    
    A project for identifying objects, scenes, and text in images other than used rag technology

---
+++ uploadfile.py
@@ -0,0 +1,30 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_qdrant import QdrantVectorStore
+from langchain_ollama import OllamaEmbeddings  
+
+
+#embeddings
+embeddings = OllamaEmbeddings(model="llama3.2")
+
+#load the document
+loader = PyPDFLoader("braintumour.pdf")
+pages = loader.load_and_split() 
+
+
+
+#url and api key
+
+url = "ENTER THE URL"  
+api_key = "ENTER THE API KEY "
+
+
+#vector store
+qdrant = QdrantVectorStore.from_documents(
+    pages,
+    embeddings,
+    url=url,
+    prefer_grpc=True,
+    api_key=api_key,
+    collection_name="braintumour",
+)
+print(pages)
